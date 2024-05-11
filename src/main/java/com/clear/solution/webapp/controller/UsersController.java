b0842ade11791bae9e4efb9c80c02706cd89e12e
Partizanin <topiciv1@yandex.ua>
2024-05-11 21:35:10 +0300

    refactoring

--- src/main/java/com/clear/solution/webapp/controller/UsersController.java
+++ src/main/java/com/clear/solution/webapp/controller/UsersController.java
@@ -1,19 +1,9 @@
 package com.clear.solution.webapp.controller;
 
-import com.clear.solution.webapp.component.Patcher;
-import com.clear.solution.webapp.exception.UserAddressNotFoundException;
-import com.clear.solution.webapp.exception.UserBirthDateNotFoundException;
-import com.clear.solution.webapp.exception.UserBirthDateRangeNotFoundException;
-import com.clear.solution.webapp.exception.UserEmailNotFoundException;
-import com.clear.solution.webapp.exception.UserFirstNameNotFoundException;
-import com.clear.solution.webapp.exception.UserIdNotFoundException;
-import com.clear.solution.webapp.exception.UserLastNameNotFoundException;
-import com.clear.solution.webapp.exception.UserPhoneNumberNotFoundException;
 import com.clear.solution.webapp.model.User;
 import com.clear.solution.webapp.service.UserService;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -48,104 +38,63 @@ public class UsersController {
 
     @GetMapping("/{id}")
     ResponseEntity<User> getUserById(@PathVariable Long id) {
-        return ResponseEntity.ok(userservice.findById(id).orElseThrow(() -> new UserIdNotFoundException(id)));
+        return userservice.findById(id);
     }
 
     @GetMapping(params = "email")
     ResponseEntity<User> getUserByEmail(@RequestParam String email) {
-        return ResponseEntity.ok(userservice.findByEmail(email).orElseThrow(() -> new UserEmailNotFoundException(email)));
+        return userservice.findByEmail(email);
     }
 
     @GetMapping(params = "firstName")
     ResponseEntity<User> getUserByFirstName(@RequestParam String firstName) {
-        return ResponseEntity.ok(userservice.findByFirstName(firstName).orElseThrow(() -> new UserFirstNameNotFoundException(firstName)));
+        return userservice.findByFirstName(firstName);
     }
 
     @GetMapping(params = "lastName")
     ResponseEntity<User> getUserByLastName(@RequestParam String lastName) {
-        return ResponseEntity.ok(userservice.findByLastName(lastName).orElseThrow(() -> new UserLastNameNotFoundException(lastName)));
+        return userservice.findByLastName(lastName);
     }
 
     @GetMapping(params = "address")
     ResponseEntity<User> getUserByAddress(@RequestParam String address) {
-        return ResponseEntity.ok(userservice.findByAddress(address).orElseThrow(() -> new UserAddressNotFoundException(address)));
+        return userservice.findByAddress(address);
     }
 
     @GetMapping(params = "phoneNumber")
     ResponseEntity<User> getUserByPhoneNumber(@RequestParam String phoneNumber) {
-        return ResponseEntity.ok(userservice.findByPhoneNumber(phoneNumber).orElseThrow(() -> new UserPhoneNumberNotFoundException(phoneNumber)));
+        return userservice.findByPhoneNumber(phoneNumber);
     }
 
     @GetMapping(params = "birthDate")
     ResponseEntity<User> getUserByBirthDate(@RequestParam LocalDate birthDate) {
-        return ResponseEntity.ok(userservice.findByBirthDate(birthDate).orElseThrow(() -> new UserBirthDateNotFoundException(birthDate)));
+        return userservice.findByBirthDate(birthDate);
     }
 
     @GetMapping(params = {"from", "to"})
     ResponseEntity<List<User>> getUserByBirthDateRange(@RequestParam LocalDate from, @RequestParam LocalDate to) {
-        if (from.isAfter(to)) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok(userservice.findByBirthDateRange(from, to).orElseThrow(() -> new UserBirthDateRangeNotFoundException(from, to)));
+        return userservice.findByBirthDateRange(from, to);
     }
 
 //    post mapping
 
     @PostMapping()
     ResponseEntity<User> createUser(@RequestBody User user) {
-        System.out.println("createUser mapping");
-        if (userservice.userAgeLessThan(user)) {
-            System.out.println("bad request");
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok(userservice.saveUser(user));
+        return userservice.createUser(user);
     }
 
     @PutMapping("/{id}")
     ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
-        System.out.println("updateUser mapping");
-        User body = userservice.findById(id).map(
-                userFromDb -> {
-                    userFromDb.setFirstName(user.getFirstName());
-                    userFromDb.setLastName(user.getLastName());
-                    userFromDb.setAddress(user.getAddress());
-                    userFromDb.setBirthDate(user.getBirthDate());
-                    userFromDb.setEmail(user.getEmail());
-                    userFromDb.setPhoneNumber(user.getPhoneNumber());
-                    return userservice.saveUser(userFromDb);
-                }
-        ).orElseThrow(() -> new UserIdNotFoundException(id));
-
-        return ResponseEntity.ok(body);
+        return userservice.updateUser(id, user);
     }
 
     @PatchMapping()
     ResponseEntity<User> patchUser(@RequestBody User patchUser) {
-        Optional<User> userByIdOpt = userservice.findById(patchUser.getId());
-
-        if (userByIdOpt.isPresent()) {
-            User userById = userByIdOpt.get();
-            try {
-                Patcher.userPatcher(userById, patchUser);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-
-            return ResponseEntity.ok(userservice.saveUser(userById));
-        } else {
-            throw new UserIdNotFoundException(patchUser.getId());
-        }
+        return userservice.patchUser(patchUser);
     }
 
     @DeleteMapping("/{id}")
     ResponseEntity<User> deleteUser(@PathVariable Long id) {
-        Optional<User> userByIdOpt = userservice.findById(id);
-
-        if (userByIdOpt.isEmpty()) {
-            throw new UserIdNotFoundException(id);
-        } else {
-            userservice.deleteUser(userByIdOpt.get());
-            return ResponseEntity.ok().build();
-        }
+        return userservice.deleteUser(id);
     }
 }
--- src/main/java/com/clear/solution/webapp/service/UserService.java
+++ src/main/java/com/clear/solution/webapp/service/UserService.java
@@ -1,5 +1,14 @@
 package com.clear.solution.webapp.service;
 
+import com.clear.solution.webapp.component.Patcher;
+import com.clear.solution.webapp.exception.UserAddressNotFoundException;
+import com.clear.solution.webapp.exception.UserBirthDateNotFoundException;
+import com.clear.solution.webapp.exception.UserBirthDateRangeNotFoundException;
+import com.clear.solution.webapp.exception.UserEmailNotFoundException;
+import com.clear.solution.webapp.exception.UserFirstNameNotFoundException;
+import com.clear.solution.webapp.exception.UserIdNotFoundException;
+import com.clear.solution.webapp.exception.UserLastNameNotFoundException;
+import com.clear.solution.webapp.exception.UserPhoneNumberNotFoundException;
 import com.clear.solution.webapp.model.User;
 import com.clear.solution.webapp.repository.UserRepository;
 import java.time.LocalDate;
@@ -8,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,43 +37,55 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public Optional<User> findById(Long id) {
-        return userRepository.findById(id);
+    public ResponseEntity<User> findById(Long id) {
+        return ResponseEntity.ok(userRepository.findById(id).
+                orElseThrow(() -> new UserIdNotFoundException(id)));
     }
 
-    public Optional<User> findByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public ResponseEntity<User> findByEmail(String email) {
+        return ResponseEntity.ok(userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserEmailNotFoundException(email)));
     }
 
-    public Optional<User> findByFirstName(String firstName) {
-        return userRepository.findByFirstName(firstName);
+    public ResponseEntity<User> findByFirstName(String firstName) {
+        return ResponseEntity.ok(userRepository.findByFirstName(firstName).
+                orElseThrow(() -> new UserFirstNameNotFoundException(firstName)));
     }
 
-    public Optional<User> findByLastName(String lastName) {
-        return userRepository.findByLastName(lastName);
+    public ResponseEntity<User> findByLastName(String lastName) {
+        return ResponseEntity.ok(userRepository.findByLastName(lastName).
+                orElseThrow(() -> new UserLastNameNotFoundException(lastName)));
+
     }
 
-    public Optional<User> findByAddress(String address) {
-        return userRepository.findByAddress(address);
+    public ResponseEntity<User> findByAddress(String address) {
+        return ResponseEntity.ok(userRepository.findByAddress(address).
+                orElseThrow(() -> new UserAddressNotFoundException(address)));
     }
 
-    public Optional<User> findByPhoneNumber(String phoneNumber) {
-        return userRepository.findByPhoneNumber(phoneNumber);
+    public ResponseEntity<User> findByPhoneNumber(String phoneNumber) {
+        return ResponseEntity.ok(userRepository.findByPhoneNumber(phoneNumber).
+                orElseThrow(() -> new UserPhoneNumberNotFoundException(phoneNumber)));
     }
 
-    public Optional<User> findByBirthDate(LocalDate birthDate) {
-        return userRepository.findByBirthDate(birthDate);
+    public ResponseEntity<User> findByBirthDate(LocalDate birthDate) {
+        return ResponseEntity.ok(userRepository.findByBirthDate(birthDate).
+                orElseThrow(() -> new UserBirthDateNotFoundException(birthDate)));
     }
 
-    public Optional<List<User>> findByBirthDateRange(LocalDate from, LocalDate to) {
-        return userRepository.findByBirthDateBetween(from, to);
+    public ResponseEntity<List<User>> findByBirthDateRange(LocalDate from, LocalDate to) {
+        if (from.isAfter(to)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(userRepository.findByBirthDateBetween(from, to)
+                .orElseThrow(() -> new UserBirthDateRangeNotFoundException(from, to)));
     }
 
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
-    public boolean userAgeLessThan(User user) {
+    private boolean userAgeLessThan(User user) {
         long years = ChronoUnit.YEARS.between(user.getBirthDate(), LocalDate.now());
         System.out.println("years: " + years);
         System.out.println("MINIMUM_YEARS_REQUIRED " + MINIMUM_YEARS_REQUIRED);
@@ -72,5 +94,58 @@ public class UserService {
 
     public void deleteUser(User user) {
         userRepository.delete(user);
+    }
+
+    public ResponseEntity<User> updateUser(Long id, User user) {
+        User body = userRepository.findById(id).map(
+                userFromDb -> {
+                    userFromDb.setFirstName(user.getFirstName());
+                    userFromDb.setLastName(user.getLastName());
+                    userFromDb.setAddress(user.getAddress());
+                    userFromDb.setBirthDate(user.getBirthDate());
+                    userFromDb.setEmail(user.getEmail());
+                    userFromDb.setPhoneNumber(user.getPhoneNumber());
+                    return userRepository.save(userFromDb);
+                }
+        ).orElseThrow(() -> new UserIdNotFoundException(id));
+
+        return ResponseEntity.ok(body);
+    }
+
+    public ResponseEntity<User> patchUser(User patchUser) {
+        Optional<User> userByIdOpt = userRepository.findById(patchUser.getId());
+
+        if (userByIdOpt.isPresent()) {
+            User userById = userByIdOpt.get();
+            try {
+                Patcher.userPatcher(userById, patchUser);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+
+            return ResponseEntity.ok(userRepository.save(userById));
+        } else {
+            throw new UserIdNotFoundException(patchUser.getId());
+        }
+    }
+
+    public ResponseEntity<User> deleteUser(Long id) {
+        Optional<User> userByIdOpt = userRepository.findById(id);
+
+        if (userByIdOpt.isEmpty()) {
+            throw new UserIdNotFoundException(id);
+        } else {
+            userRepository.delete(userByIdOpt.get());
+            return ResponseEntity.ok().build();
+        }
+    }
+
+    public ResponseEntity<User> createUser(User user) {
+        System.out.println("createUser mapping");
+        if (userAgeLessThan(user)) {
+            System.out.println("bad request");
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(userRepository.save(user));
     }
 }
